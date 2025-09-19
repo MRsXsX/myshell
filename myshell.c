175acf5f22d54b288d735d89e5409fffd2380045
Oleksii <nitrog9n54@gmail.com>
2025-09-18 20:08:11 -0500

    output to a file

--- myshell.c
+++ myshell.c
@@ -4,6 +4,8 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <fcntl.h>
+
 int main(void){
     while(1){
         char * l = NULL;
@@ -35,18 +37,54 @@ int main(void){
             }
             if(nread!=-1){
                 char *args[64];
-                int argc=0;
+                int argc=0, fargc =0, use_fargs=0;
+
                 char * token= strtok(l," ");
+                char * fargs[64];
                 
-                while(token!=NULL && argc< 63){
-                    args[argc++]= token;
+                while(token!=NULL && (argc< 63&& fargc<63)){
+                    
+                    if((strncmp(token,"-o",2)==0)){
+                        use_fargs=1;
+
+                    }
+                    else if(use_fargs){
+                        fargs[fargc++]= token;
+                    }
+                    else{
+                        args[argc++]= token;
+                    }
+
+                    
                     token = strtok(NULL, " ");
 
 
 
                 }
                 args[argc] = NULL;
+                fargs[fargc] = NULL;
+                if(use_fargs && fargc==0){
+                    printf("No output file specified after -o\n");
+                    continue;
+                }
+                if(use_fargs && fargc>1){
+                    printf("Too many arguments after -o\n");
+                    continue;
+                }
+                int fdout;
+                int saved_stdout;
+                if(use_fargs){
+                //Output to a file 
+                fdout = open(fargs[0], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+                saved_stdout = dup(1);
+                
+                
+                dup2(fdout, 1); // Redirect stdout to file
+                }
 
+
+
+                // execvp(args[0], args);
                 pid_t pid = fork();
                 if(pid==0){
                     //printf("Child [%d] executing\n",getpid());
@@ -59,6 +97,13 @@ int main(void){
 
                     int status;
                   waitpid(pid,&status,0);
+                    if(use_fargs){
+                        // Restore original stdout
+                  dup2(saved_stdout, 1);
+                    close(saved_stdout);
+                    close(fdout);
+                    }
+
                     //printf("Child [%d] finished with status %d \n",pid, WEXITSTATUS(status));
                 }
                 
